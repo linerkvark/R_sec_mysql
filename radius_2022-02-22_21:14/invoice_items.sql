8a9cd65d66bcd0e74ba805e549120f2d9c5d9c4a
ap_r_sec <kvark@line-r.ru>
2022-02-22 21:14:01 +0000

    backup_2022-02-22_21:14

---
+++ radius_2022-02-22_21:14/invoice_items.sql
@@ -0,0 +1,71 @@
+-- MySQL dump 10.19  Distrib 10.3.32-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: radius
+-- ------------------------------------------------------
+-- Server version	10.3.32-MariaDB-0ubuntu0.20.04.1-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Position to start replication or point-in-time recovery from
+--
+
+-- CHANGE MASTER TO MASTER_LOG_FILE='mysql-bin.000004', MASTER_LOG_POS=342;
+
+--
+-- GTID to start replication from
+--
+
+-- SET GLOBAL gtid_slave_pos='0-2-159';
+
+--
+-- Table structure for table `invoice_items`
+--
+
+DROP TABLE IF EXISTS `invoice_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invoice_items` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `invoice_id` int(32) NOT NULL COMMENT 'invoice id of the invoices table',
+  `plan_id` int(32) DEFAULT NULL COMMENT 'the plan_id of the billing_plans table',
+  `amount` decimal(10,2) NOT NULL DEFAULT 0.00 COMMENT 'the amount cost of an item',
+  `tax_amount` decimal(10,2) NOT NULL DEFAULT 0.00 COMMENT 'the tax amount for an item',
+  `total` decimal(10,2) NOT NULL DEFAULT 0.00 COMMENT 'the total amount',
+  `notes` varchar(128) NOT NULL COMMENT 'general notes/description',
+  `creationdate` datetime DEFAULT '0000-00-00 00:00:00',
+  `creationby` varchar(128) DEFAULT NULL,
+  `updatedate` datetime DEFAULT '0000-00-00 00:00:00',
+  `updateby` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoice_items`
+--
+
+LOCK TABLES `invoice_items` WRITE;
+/*!40000 ALTER TABLE `invoice_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invoice_items` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-22 21:14:01
---
+++ radius_2022-02-22_21:14/userinfo.sql
@@ -0,0 +1,85 @@
+-- MySQL dump 10.19  Distrib 10.3.32-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: radius
+-- ------------------------------------------------------
+-- Server version	10.3.32-MariaDB-0ubuntu0.20.04.1-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Position to start replication or point-in-time recovery from
+--
+
+-- CHANGE MASTER TO MASTER_LOG_FILE='mysql-bin.000004', MASTER_LOG_POS=342;
+
+--
+-- GTID to start replication from
+--
+
+-- SET GLOBAL gtid_slave_pos='0-2-159';
+
+--
+-- Table structure for table `userinfo`
+--
+
+DROP TABLE IF EXISTS `userinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userinfo` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) DEFAULT NULL,
+  `firstname` varchar(200) DEFAULT NULL,
+  `lastname` varchar(200) DEFAULT NULL,
+  `email` varchar(200) DEFAULT NULL,
+  `department` varchar(200) DEFAULT NULL,
+  `company` varchar(200) DEFAULT NULL,
+  `workphone` varchar(200) DEFAULT NULL,
+  `homephone` varchar(200) DEFAULT NULL,
+  `mobilephone` varchar(200) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `city` varchar(200) DEFAULT NULL,
+  `state` varchar(200) DEFAULT NULL,
+  `country` varchar(100) DEFAULT NULL,
+  `zip` varchar(200) DEFAULT NULL,
+  `notes` varchar(200) DEFAULT NULL,
+  `changeuserinfo` varchar(128) DEFAULT NULL,
+  `portalloginpassword` varchar(128) DEFAULT '',
+  `enableportallogin` int(32) DEFAULT 0,
+  `creationdate` datetime DEFAULT '0000-00-00 00:00:00',
+  `creationby` varchar(128) DEFAULT NULL,
+  `updatedate` datetime DEFAULT '0000-00-00 00:00:00',
+  `updateby` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userinfo`
+--
+
+LOCK TABLES `userinfo` WRITE;
+/*!40000 ALTER TABLE `userinfo` DISABLE KEYS */;
+INSERT INTO `userinfo` VALUES (1,'vasya','','','','','','','','','','','','','','','0','',0,'2022-02-14 20:19:00','administrator',NULL,NULL),(2,'adminpetya','','','','','','','','','','','','','','','0','',0,'2022-02-14 20:20:22','administrator','2022-02-14 20:27:52','administrator'),(3,'test','','','','','','','','','','','','','','','0','',0,'2022-02-14 21:52:05','administrator','2022-02-16 21:11:24','administrator');
+/*!40000 ALTER TABLE `userinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-02-22 21:14:01
